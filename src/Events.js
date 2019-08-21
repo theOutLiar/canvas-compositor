76d609bb34623874db61819b0155a32cbaff15b5
christopher j gore <christophergore.xyz@gmail.com>
2019-08-21 17:59:53 -0400

    failed to add docs properly, repairing that and links in readme

---
+++ src/Events.js
@@ -0,0 +1,40 @@
+
+/**
+ * The `mouseup` event label
+ */
+export const MOUSEUP = 'mouseup';
+
+/**
+ * The `mousedown` event label
+ */
+export const MOUSEDOWN = 'mousedown';
+
+/**
+ * The `mousemove` event label
+ */
+export const MOUSEMOVE = 'mousemove';
+
+/**
+ * The `mouseout` event label
+ */
+export const MOUSEOUT = 'mouseout';
+
+/**
+ * The `click` event label
+ */
+export const CLICK = 'click';
+
+/**
+ * The `keydown` event label
+ */
+export const KEYDOWN = 'keydown';
+
+/**
+ * The `keyup` event label
+ */
+export const KEYUP = 'keyup';
+
+/**
+ * The `keypress` event label
+ */
+export const KEYPRESS = 'keypress';
