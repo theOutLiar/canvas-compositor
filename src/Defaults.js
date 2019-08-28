ffe9bcbf20283d605802928fdce5b2f90f733286
christopher j gore <christophergore.xyz@gmail.com>
2019-08-28 17:39:40 -0400

    100 percent documentation coverage, some major updates to text utilities, changes to the API, so bumping up to 3.0.0. changed username on github for professional reasons

---
+++ src/Defaults.js
@@ -0,0 +1,44 @@
+/**
+ * The default color to fill shapes drawn by the canvas context
+ */
+export const FILL_STYLE = 'black';
+
+/**
+ * The default color to stroke shapes drawn by the canvas context
+ */
+export const STROKE_STYLE = 'black';
+
+/**
+ * The default line cap style for strokes drawn by the canvas. 'round' chosen to match `LINE_JOIN`, which ensures smooth meshing of vertices
+ */
+export const LINE_CAP = 'round';
+
+/**
+ * The default stroke width for shapes drawn by the canvas
+ */
+export const LINE_WIDTH = 1.0;
+
+/**
+ * The default line join style strokes drawn by the canvas. 'round' chosen for smooth meshing of vertices
+ */
+export const LINE_JOIN = 'round';
+
+/**
+ * The default miter limit ratio for mitered line joins
+ */
+export const MITER_LIMIT = 10;
+
+/**
+ * The default font for text drawn by the canvas context
+ */
+export const FONT = '10px sans-serif';
+
+/**
+ * The default text alignment for text drawn by the canvas context
+ */
+export const TEXT_ALIGN = 'start';
+
+/**
+ * The default text baseline for text drawn by the canvas context
+ */
+export const TEXT_BASELINE = 'alphabetic';
---
+++ src/TextDefaults.js
@@ -0,0 +1,47 @@
+/**
+ * Default font size
+ * @type {string}
+ */
+export const FONT_SIZE = '16px';
+
+/**
+ * Default font family
+ * @type {string}
+ */
+export const FONT_FAMILY = 'sans-serif';
+
+/**
+ * Default font style
+ * @type {string}
+ */
+export const FONT_STYLE = 'normal';
+
+/**
+ * Default font variant
+ * @type {string}
+ */
+export const FONT_VARIANT = 'normal';
+
+/**
+ * Default font weight
+ * @type {string}
+ */
+export const FONT_WEIGHT = 'normal';
+
+/**
+ * Default line height
+ * @type {string}
+ */
+export const LINE_HEIGHT = 'normal';
+
+/**
+ * Default text alignment
+ * @type {string}
+ */
+export const TEXT_ALIGN = 'start';
+
+/**
+ * Default text baseline
+ * @type {string}
+ */
+export const TEXT_BASELINE = 'alphabetic';
